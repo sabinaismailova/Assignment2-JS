394bbec7759269cdb87ceac0c3fb8d980c7f3a37
sabinaismailova <sabina.ismailova88@bcmail.cuny.edu>
2023-06-14 04:33:00 -0400

    fixed changes

--- reversingArray.js
+++ reversingArray.js
@@ -10,21 +10,4 @@ function reverse(arr){
     return revArr;
 }
 
-function reverseInPlace(arr){
-    let first = '';
-    const arrLast = arr[arr.length-1];
-    let length = arr.length;
-    let i=0;
-    while(first!=arrLast&&i<length){
-        let temp = arr[i];
-        arr[i] = arr[i+1];
-        arr[i+1] = temp;
-        first = arr[0];
-        i++;
-        length--;
-    }
-    return arr;
-}
-
 console.log(reverse(arr));
-console.log(reverseInPlace(arr));
